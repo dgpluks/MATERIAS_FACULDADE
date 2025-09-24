efd3bbd273af6c5d487855e72073ecf4dde1b741
dgpluks <danielgomespereira2006@gmail.com>
2025-09-24 15:29:00 -0300

    Create pra_acabar1.java

---
+++ AEDS-II/oficinas/pra_acabar1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class cidade{
+	public int dist;
+	public String nome;
+	public String[] viz;
+	public int qn;
+	public int tam = 1;
+	public cidade(String[] cids){
+		dist = 0;
+		nome = "BeloHorizonte";
+		qn = 1;
+		viz = new String[tam];
+	}
+	public cidade(String nom){
+		dist = -1;
+		nome = nom;
+		viz = new String[tam];
+	}
+	public add (String nom){
+		
+	}
+}
+class cm{
+	public int indice(String cid){
+		if(1==9){
+		}
+		return 0;
+	}
+	public void main(String[] args){
+		 
+	}
+}
