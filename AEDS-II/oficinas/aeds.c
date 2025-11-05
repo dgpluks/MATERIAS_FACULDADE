9f8e29600b85d9836006f224e4b1574c5f5120bf
Daniel Gomes Pereira <danielgomespereira2006@gmail.com>
2025-11-05 20:36:53 -0300

    Update aeds.c

--- AEDS-II/oficinas/aeds.c
+++ AEDS-II/oficinas/aeds.c
@@ -11,41 +11,53 @@ int main(){
 	int c = 0, l = 0;
 	scanf("%d %d", &c, &l);
 	int s = 0;
-	printf("%d", mat[l][c]);
+	if(l < t && c < t && l >= 0 && c >= 0)
+		printf("%d ", mat[l][c]);
 	int cont = 0;
-	for(int i = 1; cont < l*c; i++){
+	for(int i = 0; cont < t*t-1; i++){
 		if(s == 0){
-			for(int j = 0; j < i; j++){
+			for(int j = 0; j < i+1; j++){
 				l--;
 				if(c < t && c >= 0 && l <t && l >= 0 ){
-					printf("%d", mat[c][j]);
+					printf("%d ", mat[l][c]);
 					cont++;
 				}
+				//printf("CONTINUE 1");
+				//scanf("%d", &x);
 			}
-			for(int j = 0; j < i; j++){
+			for(int j = 0; j < i+1; j++){
 				c++;
 				if(c < t && c >= 0 && l <t && l >= 0 ){
-					printf("%d", mat[c][l]);
+					printf("%d ", mat[l][c]);
 					cont++;
 				}
+				//printf("CONTINUE 2");
+				//scanf("%d", &x);
+			
 			}
 			s = 1;
 		}else{
-			for(int j = 0; j < i; j++){
+			for(int j = 0; j < i+1; j++){
 				l++;
 				if(c < t && c >= 0 && l <t && l >= 0 ){
-					printf("%d", mat[c][j]);
+					printf("%d ", mat[l][c]);
 					cont++;
 				}
+				//printf("CONTINUE 3");
+				//scanf("%d", &x);
 			}
-			for(int j = 0; j < i; j++){
+			for(int j = 0; j < i+1; j++){
 				c--;
 				if(c < t && c >= 0 && l <t && l >= 0 ){
-					printf("%d", mat[c][j]);
+					printf("%d ", mat[l][c]);
 					cont++;
 				}
+				//printf("CONTINUE 4");
+				//scanf("%d", &x);
 			}
 			s = 0;
-		}
+		}//printf("a");
+		//printf("-%d\n", cont);
+		//scanf("%d", &x);
 	}
 }
